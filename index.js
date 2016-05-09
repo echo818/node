7f80e01af271d85b4d4b7fd0c0f205984c9e227c
echo008 <1067054231@qq.com>
2016-05-09 21:03:46 +0800

    second commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+http.createServer(function(req,res){
+	res.writeHead(200,{"Content-Type":"text/plain"});
+	res.write("hello,world");
+	res.end();
+}).listen(8000);
+
+console.log("nodejs start running 8000 port...");
